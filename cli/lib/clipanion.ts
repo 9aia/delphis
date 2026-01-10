ec9be721187b13622302e7c7297617652c4a90a5
Luis Emidio <luisfuturist@gmail.com>
2026-01-09 23:52:14 -0300

    Enhance logging capabilities in CLI commands
    - Introduced a `createLogger` function to standardize logging across commands with levels: info, warn, error, and debug.
    - Updated `BaseCommand` to utilize the new logging system, improving error handling output.
    - Created a `LogableCommand` class to extend logging functionality for commands.

--- cli/lib/clipanion.ts
+++ cli/lib/clipanion.ts
@@ -1,8 +1,40 @@
+import type { BaseContext } from 'clipanion'
 import c from 'chalk-template'
+
 import { Command } from 'clipanion'
 
-export abstract class BaseCommand extends Command {
-  async catch(error: any): Promise<void> {
-    this.context.stdout.write(c`{red Error: ${error.message}}\n`)
+export function createLogger<C extends BaseContext = BaseContext>(command: Command<C>) {
+  return {
+    info(message: string) {
+      command.context.stdout.write(c`[{green INFO}] ${message}\n`)
+    },
+
+    warn(message: string) {
+      command.context.stdout.write(c`[{yellow WARN}] ${message}\n`)
+    },
+
+    error(message: string) {
+      command.context.stderr.write(c`[{red ERROR}] ${message}\n`)
+    },
+
+    debug(message: string) {
+      if (Bun.env.NODE_ENV !== 'development') {
+        return
+      }
+
+      command.context.stdout.write(c`[{gray DEBUG}] ${message}\n`)
+    },
+  }
+}
+
+export abstract class LogableCommand<C extends BaseContext = BaseContext> extends Command<C> {
+  public logger = createLogger(this)
+}
+
+export abstract class BaseCommand<C extends BaseContext = BaseContext> extends LogableCommand<C> {
+  async catch(error: unknown) {
+    this.logger.error(`Internal error: ${error instanceof Error
+      ? error.message
+      : String(error)}`)
   }
 }
