193680419933b879691c11d170a4ebe792bdde87
vinicenter <viniemidiobosi@gmail.com>
2026-01-10 00:15:54 -0300

    Add Tailscale integration with CLI commands and update dependencies

--- cli/commands/join.ts
+++ cli/commands/join.ts
@@ -1,11 +1,27 @@
 import { Option } from 'clipanion'
 import { BaseCommand } from '../lib/clipanion'
+import { getTailscaleIp, isTailscaleInstalled, isTailscaleUp } from '../lib/tailscale'
 
 export class JoinCommand extends BaseCommand {
   name = Option.String()
-  newWindow = Option.Boolean('-n,--new-window', false)
 
   async execute() {
     this.context.stdout.write(`Hello ${this.name}!\n`)
+
+    const tailscaleInstalled = await isTailscaleInstalled()
+
+    if (!tailscaleInstalled) {
+      this.logger.error('Tailscale is not installed. Please install Tailscale and try again.')
+    }
+
+    const tailscaleUp = await isTailscaleUp()
+
+    if (!tailscaleUp) {
+      this.logger.error('Tailscale is not up. Please start Tailscale and try again.')
+    }
+
+    const ip = getTailscaleIp()
+
+    this.context.stdout.write(`Tailscale IP: ${ip}\n`)
   }
 }
